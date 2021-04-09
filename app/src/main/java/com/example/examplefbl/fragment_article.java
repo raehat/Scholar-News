3a2d3eccf729692bcf74e0f7bee22dd8d8717faa
Jatin7385 <jats7385@gmail.com>
2021-04-10 01:48:07 +0530

    RecyclerView Commit

--- app/src/main/java/com/example/examplefbl/fragment_article.java
+++ app/src/main/java/com/example/examplefbl/fragment_article.java
@@ -14,6 +14,7 @@ public class fragment_article extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_article,container,false);
     }
 }
