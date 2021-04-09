ae649cc66e00fd3949d121aada1f73585974b15b
Shubham Maheshwari <77975234+shubhamghb@users.noreply.github.com>
2021-04-09 23:59:08 +0530

    NavBar

--- app/src/main/java/com/example/examplefbl/EditorPage.java
+++ app/src/main/java/com/example/examplefbl/EditorPage.java
@@ -6,6 +6,14 @@ import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EditorPage extends AppCompatActivity {
 
@@ -13,6 +21,10 @@ public class EditorPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor_page);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
 
         /*hey*/
         @SuppressLint("WrongConstant") SharedPreferences sh
@@ -25,4 +37,26 @@ public class EditorPage extends AppCompatActivity {
         myEdit.commit();
         Toast.makeText(this, "" + sh.getString("username", "lll"), Toast.LENGTH_SHORT).show();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch(item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_article:
+                            selectedFragment = new fragment_article();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new accountfragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
