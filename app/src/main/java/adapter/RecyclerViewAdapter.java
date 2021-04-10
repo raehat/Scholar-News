2f2ce9d0d11dcc370f797fd9111c88ca4bff4150
Jatin7385 <jats7385@gmail.com>
2021-04-10 10:09:02 +0530

    RecyclerView Final Commit

--- app/src/main/java/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/adapter/RecyclerViewAdapter.java
@@ -1,4 +1,53 @@
 package adapter;
 
-public class RecyclerViewAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.examplefbl.R;
+import com.example.examplefbl.datamodel;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.myviewholder> {
+    ArrayList<datamodel> dataholder;
+
+    public RecyclerViewAdapter(ArrayList<datamodel> dataholder) {
+        this.dataholder = dataholder;
+    }
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview,parent,false);
+        return new myviewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myviewholder holder, int position) {
+        holder.img.setImageResource(dataholder.get(position).getImage());
+        holder.header.setText(dataholder.get(position).getHeader());
+        holder.desc.setText(dataholder.get(position).getDesc());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataholder.size();
+    }
+
+    class myviewholder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView header,desc;
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.icon_img1);
+            header = itemView.findViewById(R.id.header);
+            desc = itemView.findViewById(R.id.description);
+        }
+    }
 }
--- app/src/main/java/com/example/examplefbl/fragment_article.java
+++ app/src/main/java/com/example/examplefbl/fragment_article.java
@@ -9,12 +9,57 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import adapter.RecyclerViewAdapter;
 
 public class fragment_article extends Fragment {
+    RecyclerView recyclerview;
+    ArrayList<datamodel> dataholder;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_article,container,false);
+        View view= inflater.inflate(R.layout.fragment_article,container,false);
+        recyclerview=view.findViewById(R.id.recyclerView);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
+        dataholder = new ArrayList<>();
+
+        datamodel ob1=new datamodel(R.drawable.testimg,"Angular","Web Application");
+        dataholder.add(ob1);
+
+        datamodel ob2=new datamodel(R.drawable.testimg,"C Programming","Embed Programming");
+        dataholder.add(ob2);
+
+        datamodel ob3=new datamodel(R.drawable.testimg,"C++ Programming","Embed Programming");
+        dataholder.add(ob3);
+
+        datamodel ob4=new datamodel(R.drawable.testimg,".NET Programming","Desktop and Web Programming");
+        dataholder.add(ob4);
+
+        datamodel ob5=new datamodel(R.drawable.testimg,"Java Programming","Desktop and Web Programming");
+        dataholder.add(ob5);
+
+        datamodel ob6=new datamodel(R.drawable.testimg,"Magento","Web Application Framework");
+        dataholder.add(ob6);
+
+        datamodel ob7=new datamodel(R.drawable.testimg,"NodeJS","Web Application Framework");
+        dataholder.add(ob7);
+
+        datamodel ob8=new datamodel(R.drawable.testimg,"Python","Desktop and Web Programming");
+        dataholder.add(ob8);
+
+        datamodel ob9=new datamodel(R.drawable.testimg,"Shopify","E-Commerce Framework");
+        dataholder.add(ob9);
+
+        datamodel ob10=new datamodel(R.drawable.testimg,"Wordpress","WebApplication Framewrok");
+        dataholder.add(ob10);
+
+        recyclerview.setAdapter(new RecyclerViewAdapter(dataholder));
+
+        return view;
     }
 }
